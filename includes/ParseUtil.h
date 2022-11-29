984746fa780190ca70065a7e1112bcfb436ebae8
Jacob Shalabi <jshalabi03@gmail.com>
2022-11-29 14:52:23 -0600

    Added more parsing utility to detect jump instructions, fixed trim functions

--- includes/ParseUtil.h
+++ includes/ParseUtil.h
@@ -5,16 +5,22 @@
 #include <stdexcept>
 #include <fstream>
 #include <iostream>
+#include <unordered_map>
 
-using namespace std;
+namespace ParseUtil {
+
+static std::unordered_map<char, int> is_white_space = {{' ', 1},
+                                                       {'\n', 1},
+                                                       {'\t', 1},};
 
 std::string TrimRight(const std::string & str);
 std::string TrimLeft(const std::string & str);
 std::string Trim(const std::string & str);
 
-// returns cleaned version of
+// returns cleaned version of line
 std::string Clean(const std::string &str);
 
+// returns true if line is a label
 bool IsLabel(const std::string &str);
 
 bool IsNonLabelInstruction(const std::string &str);
@@ -24,4 +30,11 @@ bool IsComment(const std::string &str);
 // gets "main" from "main:"
 std::string GetLabelString(const std::string &str);
 
-vector<string> GetLines(string filename);
+std::vector<std::string> GetLines(std::string filename);
+
+// gets {"b", ".LBB0_3"} from "b    .LBB0_3"
+std::pair<std::string,std::string> GetInstruction(const std::string &str);
+
+bool IsJumpInstruction(const std::string &str);
+
+};
--- src/ParseUtil.cpp
+++ src/ParseUtil.cpp
@@ -1,13 +1,23 @@
 #include "ParseUtil.h"
 
+namespace ParseUtil {
+
 std::string TrimRight(const std::string & str) {
     std::string tmp = str;
-    return tmp.erase(tmp.find_last_not_of(" ") + 1);
+    while (!tmp.empty() && is_white_space[tmp.back()]) {
+        tmp.pop_back();
+    }
+    return tmp;
+    // return tmp.erase(tmp.find_last_not_of(" ") + 1);
 }
 
 std::string TrimLeft(const std::string & str) {
     std::string tmp = str;
-    return tmp.erase(0, tmp.find_first_not_of(" "));
+    while (!tmp.empty() && is_white_space[tmp.front()]) {
+        tmp.erase(0,1);
+    }
+    return tmp;
+    // return tmp.erase(0, tmp.find_first_not_of(" "));
 }
 
 std::string Trim(const std::string & str) {
@@ -33,21 +43,22 @@ bool IsNonLabelInstruction(const std::string &str) {
 }
 
 bool IsComment(const std::string &str) {
-    return str.length() > 2 && str[0] == '/' && str[1] == '/';
+    std::string tmp = Trim(str);
+    return tmp.length() > 2 && tmp[0] == '/' && tmp[1] == '/';
 }
 
 std::string GetLabelString(const std::string &str) {
     return str.substr(0, str.length() - 1);
 }
 
-vector<string> GetLines(string filename) {
-    string line;
-    vector<string> res;
+std::vector<std::string> GetLines(std::string filename) {
+    std::string line;
+    std::vector<std::string> res;
 
-    ifstream ifs(filename);
+    std::ifstream ifs(filename);
     if (!ifs.is_open()) throw std::runtime_error("Bad IO");
 
-    while (getline(ifs, line)) {
+    while (std::getline(ifs, line)) {
         res.push_back(Trim(line));
     }
 
@@ -55,3 +66,45 @@ vector<string> GetLines(string filename) {
 
     return res;
 }
+
+std::pair<std::string,std::string> GetInstruction(const std::string &str) {
+    std::string cpy(Trim(str));
+    size_t pos = cpy.find(' ');
+    if (pos == std::string::npos) {
+        std::cout << "HERE" << std::endl;
+        return {cpy, ""};
+    }
+    else return {cpy.substr(0,pos), Trim(cpy.substr(pos))};
+}
+
+// @param str - trimmed instruction string, no params
+// @returns true if instrution is a jump instruction, false otherwise
+bool IsJumpInstruction(const std::string &str) {
+    return ((str.length() == 1 && str[0] == 'b')
+     || (str.length() > 1 && str[0] == 'b' && str[1] == '.'));
+}
+
+// std::pair<std::string,std::vector<std::string>> GetInstructionDetails(const std::string &str) {
+//     string instruction;
+//     vector<string> params;
+
+//     size_t pos = str.find(' ');
+//     if (pos == string::npos) return {str, {}};
+
+//     instruction = str.substr(0,pos++);
+//     string curr_instruction;
+//     while (pos < str.length() && pos != string::npos) {
+//         if (str[pos] == ',') {
+//             params.push_back(curr_instruction);
+//             curr_instruction.clear();
+//             pos += 2;
+//         } else {
+//             curr_instruction += str[pos];
+//             ++pos;
+//         }
+//     }
+//     params.push_back(curr_instruction);
+//     return {instruction, params};
+// }
+
+};
